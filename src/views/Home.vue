47c3acb0af9bd8b115ca65000d7c7ca31e8c896b
Sumit Pal <sumit.pal@freshlycosmetics.com>
2021-09-08 20:39:24 +0200

    Add Tabs to homepage.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,34 +2,53 @@
   <div class="home">
     <div class="container">
       <div class="search-container">
-        <InputField v-model="data.searchValue"
-               @change="search"
-               :style="getSearchInputBackgroundStyles()"
-               type="text"
-               aria-label="Search"
-               place-holder="Search albums, tracks and artists"/>
+        <InputField v-model="searchValue"
+                    @change="search"
+                    :style="getSearchInputBackgroundStyles()"
+                    type="text"
+                    aria-label="Search"
+                    place-holder="Search albums, tracks and artists"/>
+      </div>
+
+      <div class="tabs">
+        <!-- Tabs !-->
+        <Tabs :tabs="tabs"
+              :active-tab="activeTab"
+              @update="updateActiveTab">
+        </Tabs>
+
+        <!-- Tab contents Albums !-->
+        <div v-for="tab in tabs"
+             :key="tab.name">
+          <AlbumList :items="tab.list"
+                     v-if="activeTab === tab.name">
+          </AlbumList>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { ref } from 'vue';
 import InputField from '../components/form/InputField.vue';
+import AlbumList from '../components/AlbumList.vue';
+import Tabs from '../components/ui/Tabs.vue';
 import magnifyingGlassIcon from '../assets/img/lupa.png';
 
 export default {
   name: 'Home',
   components: {
     InputField,
+    AlbumList,
+    Tabs,
   },
   setup() {
-    const data = reactive({
-      searchValue: '',
-    });
+    // Search
+    const searchValue = ref('');
 
     const search = () => {
-      console.log(data.searchValue);
+      console.log(searchValue.value);
     };
 
     const getSearchInputBackgroundStyles = () => ({
@@ -39,15 +58,45 @@ export default {
       backgroundPositionX: '16px',
     });
 
-    return { data, search, getSearchInputBackgroundStyles };
+    // Tabs
+    const activeTab = ref('Albums');
+    const tabs = ref([
+      {
+        name: 'Albums',
+        list: [],
+      },
+      {
+        name: 'Artists',
+        list: [],
+      },
+      {
+        name: 'Tracks',
+        list: [],
+      },
+    ]);
+
+    const updateActiveTab = (newActiveTab) => {
+      activeTab.value = newActiveTab.name;
+    };
+
+    return {
+      searchValue,
+      search,
+      getSearchInputBackgroundStyles,
+      activeTab,
+      tabs,
+      updateActiveTab,
+    };
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../assets/scss/mixin/responsive-breakpoints.scss";
+
 .home {
   margin-top: 50px;
+
   .search-container {
     width: 100%;
     margin: auto;
@@ -55,6 +104,12 @@ export default {
     @include for-tablet-up {
       width: 30%;
     }
+  }
+
+  .tabs {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 }
 </style>
